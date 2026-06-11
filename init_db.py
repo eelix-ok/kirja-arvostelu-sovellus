f76b99081e48ea483d1fde3b85602220609037a6
eelix-ok <niironeneeli@gmail.com>
2026-06-11 19:30:47 +0300

    Updated init_db.py

--- init_db.py
+++ init_db.py
@@ -2,9 +2,38 @@ import sqlite3
 
 con = sqlite3.connect("database.db")
 
-with open("schema.sql", "r") as f:
-    con.executescript(f.read())
+cur = con.cursor()
 
+cur.executescript("""
+DROP TABLE IF EXISTS review_genres;
+DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    username TEXT UNIQUE,
+    password_hash TEXT
+);
+
+CREATE TABLE reviews (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    review TEXT
+);
+
+CREATE TABLE genres (
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE
+);
+
+CREATE TABLE review_genres (
+    review_id INTEGER,
+    genre_id INTEGER
+);
+""")
+
+con.commit()
 con.close()
 
-print("Database initialized!")
+print("Database created!")
